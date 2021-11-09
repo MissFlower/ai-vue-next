9a6abc4a2a6dcb1a2a16e3c951104f09777e9b06
aidongyang <aidongyang@vsulv.com>
2021-11-09 16:56:27 +0800

    mchore1: 集成@commitlint/config-conventional&@commitlint/cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-const DOMGlobals = ['window', 'document']
-const NodeGlobals = ['module', 'require']
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -11,16 +9,6 @@ module.exports = {
       // we are only using this rule to check for unused arguments since TS
       // catches unused variables but not args.
       { varsIgnorePattern: '.*', args: 'none' }
-    ],
-    // most of the codebase are expected to be env agnostic
-    'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
-    // since we target ES2015 for baseline support, we need to forbid object
-    // rest spread usage (both assign and destructure)
-    'no-restricted-syntax': [
-      'error',
-      'ObjectExpression > SpreadElement',
-      'ObjectPattern > RestElement',
-      'AwaitExpression'
     ]
   }
 }
