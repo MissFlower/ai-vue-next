7848dc72d43fd399fb8f1c3f95cdc7d378429080
aidongyang <aidongyang@vsulv.com>
2021-11-09 15:36:42 +0800

    chore1: 集成husky&eslint&prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const DOMGlobals = ['window', 'document']
+const NodeGlobals = ['module', 'require']
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': [
+      'error',
+      // we are only using this rule to check for unused arguments since TS
+      // catches unused variables but not args.
+      { varsIgnorePattern: '.*', args: 'none' }
+    ],
+    // most of the codebase are expected to be env agnostic
+    'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
+    // since we target ES2015 for baseline support, we need to forbid object
+    // rest spread usage (both assign and destructure)
+    'no-restricted-syntax': [
+      'error',
+      'ObjectExpression > SpreadElement',
+      'ObjectPattern > RestElement',
+      'AwaitExpression'
+    ]
+  }
+}
