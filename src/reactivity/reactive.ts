9de3eb97b593e536ef6c66d162f060b1f874ebc9
aidongyang <aidongyang@vsulv.com>
2021-11-09 11:02:01 +0800

    feat: 实现readonly

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -15,3 +15,16 @@ export function reactive(raw) {
     }
   })
 }
+
+export function readonly(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      const res = Reflect.get(target, key)
+      return res
+    },
+    set(target) {
+      console.warn(`target 是 readonly，不能set, ${target}`)
+      return true
+    }
+  })
+}
---
+++ src/reactivity/tests/readonly.spec.ts
@@ -0,0 +1,26 @@
+import { readonly } from "../reactive";
+
+describe('readonly', () => {
+  it('happy path', () => {
+    const original = {
+      foo: 1,
+      bar: {
+        baz: 2
+      }
+    }
+    const wrap = readonly(original)
+    expect(wrap).not.toBe(original)
+    expect(wrap.foo).toBe(1)
+  });
+
+  it('warn then set called', () => {
+    console.warn = jest.fn()
+    const user = readonly({
+      age: 10
+    })
+
+    user.age = 11
+    expect(user.age).toBe(10)
+    expect(console.warn).toBeCalled()
+  });
+});
