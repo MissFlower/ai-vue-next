348a4d59c43032544806d7dc6739cb7c0459b54e
ady <DoveyLoveyCora@163.com>
2022-08-08 20:34:32 +0800

    feat: 实现transfrom功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,32 @@
+import { NodeTypes } from './ast'
+
+export function transform(root, options) {
+  const context = createTransformContext(root, options)
+  traverseNode(root, context)
+}
+
+function createTransformContext(root: any, options: any): any {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
+  }
+
+  return context
+}
+
+function traverseNode(node: any, context) {
+  const { nodeTransforms } = context
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  const { children } = node
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      // 深度优先搜索
+      traverseNode(node, context)
+    }
+  }
+}
